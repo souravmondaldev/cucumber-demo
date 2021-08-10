f603def58c4abe227d35224a77034a8bd7813a87
Sourav Mondal <souravmondal0341@gmail.com>
2021-08-10 11:16:38 +0530

    Update RunCucumberTest.java

--- src/test/java/com/sapient/trg/RunCucumberTest.java
+++ src/test/java/com/sapient/trg/RunCucumberTest.java
@@ -2,8 +2,8 @@ package com.sapient.trg;
 
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.platform.engine.Cucumber;
-
-@Cucumber
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/resources",
 		glue = "com.sapient.trg",
